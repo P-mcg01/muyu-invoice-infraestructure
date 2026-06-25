104d10ec2a614333662954d15309f3a5b9535c33
P-mcg01 <pablo.cg0806@gmail.com>
2026-06-24 22:57:04 -0400

    refactor(terraform): replace VPC resources with terraform-aws-vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,82 +1,19 @@
-# VPC resources will be defined here when network infrastructure is implemented.
-resource "aws_vpc" "muyu" {
-  cidr_block = "10.4.0.0/16"
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.1"
 
-# Public subnet resources will be added here for internet-facing components.
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.muyu.id
-  cidr_block = "10.4.1.0/24"
-}
+  name = "muyu"
+  cidr = "10.4.0.0/16"
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id     = aws_vpc.muyu.id
-  cidr_block = "10.4.2.0/24"
-}
+  azs = ["us-east-1a", "us-east-1b"]
 
-# Private subnet resources will be added here for internal components.
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.muyu.id
-  cidr_block = "10.4.3.0/24"
-}
+  public_subnets = ["10.4.1.0/24", "10.4.2.0/24"]
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id     = aws_vpc.muyu.id
-  cidr_block = "10.4.4.0/24"
-}
+  private_subnets = ["10.4.3.0/24", "10.4.4.0/24"]
 
-# Internet Gateway resources will be added here for public internet access.
-resource "aws_eip" "nat" {
-  domain = "vpc"
-}
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_subnet_1.id
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.muyu.id
-}
-
-# Route Tables will be added here for public and private subnet routing.
-resource "aws_route_table" "rt_public" {
-  vpc_id = aws_vpc.muyu.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table" "rt_private" {
-  vpc_id = aws_vpc.muyu.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
-}
-
-# Route Table Associations will be added here to attach subnets to routes.
-resource "aws_route_table_association" "public_subnet_1_association" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.rt_public.id
-}
-
-resource "aws_route_table_association" "public_subnet_2_association" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.rt_public.id
-}
-
-resource "aws_route_table_association" "private_subnet_1_association" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.rt_private.id
-}
-
-resource "aws_route_table_association" "private_subnet_2_association" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.rt_private.id
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
