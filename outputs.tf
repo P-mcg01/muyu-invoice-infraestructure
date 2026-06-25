f8a89dda4af92017ac6f762fbba6d12bd7f58ff4
P-mcg01 <pablo.cg0806@gmail.com>
2026-06-24 23:05:28 -0400

    fix(terraform): correct VPC module references in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 # Reserved for future Terraform outputs such as VPC and subnet identifiers.
 output "vpc_id" {
-  value = module.muyu_vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnets" {
-  value = module.muyu_vpc.public_subnets
+  value = module.vpc.public_subnets
 }
 
 output "private_subnets" {
-  value = module.muyu_vpc.private_subnets
+  value = module.vpc.private_subnets
 }
 
 output "nat_public_ips" {
-  value = module.muyu_vpc.nat_public_ips
+  value = module.vpc.nat_public_ips
 }
